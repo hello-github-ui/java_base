3425a389cf391209025a38689fe4bffc43460061
hello-github-ui <util.you.come@gmail.com>
2022-01-17 23:31:07 +0800

    http 实现 get 和 post 请求并获取json响应格式内容

--- http/src/main/java/HttpTest.java
+++ http/src/main/java/HttpTest.java
@@ -1,9 +1,15 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
@@ -44,5 +50,58 @@ public class HttpTest {
     public void test1() {
         String resp = getResp("https://www.baidu.com");
         System.out.println(resp);
+    }
+
+
+    /**
+     * GET 请求获取响应结果(json格式的返回格式)
+     */
+    public String httpGet(String urlStr) {
+        String str = "";
+        try {
+//            String urlStr = "https://www.dmoe.cc/random.php?return=json";
+            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            HttpGet httpGet = new HttpGet(urlStr);
+            //获取 GET 请求响应值
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            //将请求响应值转换为String类型
+            // InputStream content = response.getEntity().getContent(); // 如果你只想获取响应内容，通过此方式
+            String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
+            //将String类型转换为json对象
+            JSONObject responseJson = JSON.parseObject(responseString);
+            //获取对应的value值
+            System.out.println(responseJson);
+            str = responseJson.get("imgurl").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    @Test
+    public void httpGetTest() {
+        httpGet("https://www.dmoe.cc/random.php?return=json");
+    }
+
+    /**
+     * POST 请求获取响应结果（JSON返回格式）
+     */
+    public void httpPost(String urlStr) {
+        try {
+//            String urlStr = "https://www.dmoe.cc/random.php?return=json";
+            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost httpPost = new HttpPost(urlStr);
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+            String responseString = EntityUtils.toString(response.getEntity());
+            JSONObject responseJson = JSON.parseObject(responseString);
+            System.out.println(responseJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void httpPostTest() {
+        httpPost("https://www.dmoe.cc/random.php?return=json");
     }
 }
