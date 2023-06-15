719f29e18a8d619e8f03856b28f6ddc2690d6361
hello-github-ui <util.you.come@gmail.com>
2023-06-15 13:51:39 +0800

    java-core-2

--- string/src/main/java/StringBuilderTest.java
+++
@@ -1,9 +0,0 @@
-public class StringBuilderTest {
-
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("123,");
-        String substring = sb.substring(0, sb.length() - 1);
-        System.out.println(substring);
-    }
-}
